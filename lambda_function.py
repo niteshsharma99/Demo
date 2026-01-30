973f401a25c3a228149a5af5832da3b7c3fb5b12
Nitesh Sharma <75922520+niteshsharma99@users.noreply.github.com>
2026-01-30 12:13:32 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,1 +1,23 @@
+import boto3
+import os
 
+ec2 = boto3.client("ec2")
+sns = boto3.client("sns")
+
+INSTANCE_ID = os.environ["INSTANCE_ID"]
+SNS_TOPIC_ARN = os.environ["SNS_TOPIC_ARN"]
+
+def lambda_handler(event, context):
+    ec2.reboot_instances(InstanceIds=[INSTANCE_ID])
+
+    message = f"EC2 instance {INSTANCE_ID} rebooted by Lambda"
+    sns.publish(
+        TopicArn=SNS_TOPIC_ARN,
+        Subject="EC2 Restart Triggered",
+        Message=message
+    )
+
+    return {
+        "statusCode": 200,
+        "body": message
+    }
