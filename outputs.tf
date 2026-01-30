4e0dbf0a43aa0aebdb6151415c5cc84da2f2eca4
Nitesh Sharma <75922520+niteshsharma99@users.noreply.github.com>
2026-01-30 12:09:49 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,11 @@
-This is also from DevOps Training.
+output "ec2_instance_id" {
+  value = aws_instance.demo_ec2.id
+}
+
+output "lambda_name" {
+  value = aws_lambda_function.restart_lambda.function_name
+}
+
+output "sns_topic_arn" {
+  value = aws_sns_topic.alert_topic.arn
+}
